c00adce13ff27bcf83a810394cf320dfd7c44ee9
seongbin.9786 <seongbin9786@gmail.com>
2017-11-05 15:49:15 +0900

    home.jsp 한글 깨짐 fix

--- src/main/java/mesh/controller/HomeController.java
+++ src/main/java/mesh/controller/HomeController.java
@@ -32,9 +32,8 @@ public class HomeController {
 		
 		String formattedDate = dateFormat.format(date);
 		
-		model.addAttribute("serverTime", formattedDate );
+		model.addAttribute("serverTime", formattedDate);
 		
 		return "home";
-	}
-	
+	}	
 }
